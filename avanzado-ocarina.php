df960c35a747f7f595d5324d2f253e39e63c3b83
BlackKoder <chrysxdzer0@gmail.com>
2023-05-06 00:33:29 -0430

    Primer commit de Games Oddysey

---
+++ avanzado-ocarina.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Games Oddysey| ¿Es realmente Ocarina of Time un juego avanzado para su época?</title>
+
+  <!--
+    - favicon
+  -->
+  <link rel="shortcut icon" href="./assets/images/ico.ico" type="image/x-icon">
+
+  <!--
+    - custom css link
+  -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+  <!--
+    - google font link
+  -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@500&display=swap" rel="stylesheet">  
+</head>
+
+<body class="light-theme">
+
+  <!--
+    - #HEADER
+  -->
+
+  <?php include('header_articulo.php') ?>
+
+
+
+
+
+  <main class="main-article">
+
+    <!--
+      - #HERO SECTION
+    -->
+
+
+      <div class="container-all" id="move-content">
+        <div class="container-cover avanzado">
+          <div class="container-info-cover">
+              <h1>¿Ocarina of Time un juego avanzado para su época?</h1>
+              <p>¿Una idea revolucionaria por parte de Nintendo o uno más del montón?.</p>
+          </div>
+        </div>  
+
+
+
+
+
+
+
+
+      
+
+        <!--
+          - BLOG SECTION
+        -->
+
+        <div class="container-content">
+        <article>
+
+            <h1>¿Es realmente Ocarina of Time un juego avanzado para su época?</h1>
+
+            <img src="./assets/images/avanzado-ocarina.png">
+
+            <p>¿Cuántos que hayan jugado el juego en su infancia lo recuerda como un mal juego?, por donde lo veas es un juego que ha sido una obra maestra y que marcó un hito en la etapa de los videojuegos en el salto al 3D. <br><br>Es cierto que ya había prototipos de otros juegos que querían dar el salto al 3D en esa época, sin embargo, nunca se hubiera implementado cada una de las mecánicas y libertad que se planteó en el Ocarina of Time. Ni siquiera en el Mario 64, ya que este tenía ciertas limitaciones en cuánto a la libertad, jugabilidad y mecánicas. Que, sin embargo, también fue un juego muy representativo y fue el punto de partida de creación de Zelda Ocarina of Time, puesto que esté poseía las mecánicas base que se implementarían en el mundo del juego para que se viera más vivo y gozara de más libertad. Igualmente con el tema de la optimización y el rendimiento.</p>
+
+            <h2 class="subtitulo">Historia y narrativa</h2>
+
+            <img src="./assets/images/historia-ocarina.png">
+
+            <p>Zelda en ese tiempo poseía una narrativa y una filosofía que quizás en ese tiempo no hubiera sido muy entendida, ya que la mayoría de las atracciones principales estaban en la interacción del jugador con el mundo y los personajes del juego, la jugabilidad y la estética es lo que marcaba la importancia en el desarrollo de Nintendo para ese juego. Sin embargo, eso no quiere decir que el juego no esté bien estructurado en sus guiones o historias. <br><br>Ha cumplido con muchas otras cosas que otros juegos no contaban para su época. Desde la aparición de líneas de tiempo que crearan una bifurcación en toda la cronología de la saga y haciendo que un juego se pudiera conectar en cuánto a historia de una u otra forma o incluso llegando a hacer referencias entre un juego y otro en algunos casos.</p>
+
+                <h2 class="subtitulo">Arte y diseño del juego</h2>
+
+                <img src="./assets/images/artwork-ocarina.jpg">
+
+                <p>El arte y diseño del juego estaban construidos precisamente para hacer que el jugador se viera arraigado al ambiente de ese mundo, que realmente gozara de esa libertad y se sintiera que estaba en un mundo que realmente estaba vivo. Quisieron innovar con algunos aspectos de los personajes, cambiando así el diseño de Link para que se viera mucho más atractivo a la vista de los jugadores, los ambientes, todos los lugares y cosas coloridas que supieron incluir a la narrativa del juego fue un gran recurso para proyectarle a uno una gran aventura épica. <br><br>Incluso el juego contaba con mejores gráficos que algunos juegos que hayan salido para la misma consola, como en el caso de Resident Evil 2, que al final tuvo que hacer un salto a la consola de la PlayStation porque no podían hacer que esos diseños concordarán con el procesamiento del hardware de la consola. Cosa que no pasa en el Ocarina, muchos podrían pensar que posiblemente sea porque los diseños de los personajes de Resident Evil buscaban de dar un aire más realista y hacer muchos detalles a un diseño poligonal terminó haciendo algo desalentador.</p>
+
+                <img src="./assets/images/artwork-ocarina-batalla.jpeg">
+
+                <p>Pero eso no quiere decir que haya sido un mal juego, al contrario, Resident Evil 2 también fue un buen juego para su época, pero precisamente Nintendo 64 no fue la mejor consola que hizo que destacara entre otros juegos. Cosa que en Ocarina of Time no pasó, ya que Nintendo se centró más el diseño de un juego que tuviera las características bien a medida para esa consola, y qué mejor juego que el icónico Ocarina of Time?. De hecho, hago hincapié en que Mario fue una gran referencia para el diseño de gran parte de los mundos de Zelda por el hecho de que le dio ese ambiente de fantasía y mundo colorido, pero luego le dieron ese toque o trasfondo más filosófico que pudo atrapar a muchos jugadores de la saga.</p>
+
+                <h2 class="subtitulo">Mecánicas y jugabilidad</h2>
+
+                <img src="./assets/images/jugabilidad-ocarina.png">
+
+                <p>Es cierto que la Nintendo 64 fue una consola revolucionaria para su época antes de que saliera la PlayStation de Sony. Ya que le dio un gran golpe a las antiguas consolas de Sega e innovó y mejoró muchos aspectos de las características de mundo 3D de los videojuegos. Claro que no tuviera el mismo tipo de éxito económico que la competencia, bien se debe a los cartuchos, porque hacían el coste de producción más caro, pero eso no quita que fuera un invento revolucionario. <br><br>Es por eso que cuando decidieron ponerse manos a la obra con el título de Ocarina of Time, buscaron de aprovechar y sacarle el máximo potencial a todo el hardware, ya que la comodidad y jugabilidad se enfoca en la comodidad de sus controles y la practicidad que te dan por ejemplo con los botones C, para que pudieras predefinir los objetos que ibas a tener en la aventura, o con el sistema de Z-targeting que integraría Nintendo para el juego, fueron ideas que mejoraron por ejemplo de lo que aprendieron de Mario 64, ya que se habían dado cuenta de lo difícil que se hacía derrotar algunos enemigos con la gravedad que se había programado en el juego y en Ocarina decidieron mejorar varios aspectos de la cámara para que no pasaras por los mismos problemas que en Mario 64.</p>
+
+                <img src="./assets/images/z-targeting.png">
+
+                <p>Pero no solo eso, tenemos que admitir que las mecánicas que lograron implementar en los juegos eran realmente superiores a los demás juegos que estaban contemporáneos e incluso los pasados. El juego te da tanta libertad que te deja montar a caballo, tiene su sistema de comercio, puedes pescar, trepar, combatir, existe la transición del día y la noche, la fijación del objetivo en el combate, puedes tocar un instrumento, explorar un mundo amplio, cambiar la cámara a primera persona, puedes utilizar muchos tipos de armas como lo son el gancho y el arco y flecha, destruir elementos del escenario con las bombas, tienes un mapa para guiarte en la zona donde estés. Realmente es una basta variedad de elementos que lo convirtieron en el juegazo de su año.</p>
+
+                <img src="./assets/images/jugabilidad2-ocarina.png">
+
+                <h2 class="subtitulo">La Música</h2>
+
+                <img src="./assets/images/musica-ocarina.png">
+
+                <p>Realmente tiene una gran profundidad la mayoría de las bandas sonoras que se crearon para la mayoría de momentos del juego, los temas dedicados a algún personaje en específico, realmente la música fue algo que el compositor Koji Kondo supo manejar para que cuadrara con la narrativa la jugabilidad del juego y que fuera muy acertada con las situaciones del juego, igualmente que pudiera ser soportada en el hardware de la Nintendo 64, un gran desafío, ya que para ese entonces buscaban de hacer hasta lo imposible para la optimización del juego y fue logrado junto con los demás elementos implementarlos en el cartucho de su época.</p>
+
+                <p>Entonces, al final, seguirá marcando un punto de referencia para la mayoría de juegos de esta época y podemos denotar que todos los patrones que ha seguido Ocarina of Time a lo largo de su travesía por querer convertirse en uno de los mejores juegos lo ha convertido realmente en una leyenda.</p>
+        </article>
+
+
+
+
+
+        <!--
+          - ASIDE
+        -->
+
+        <div class="container-aside"> 
+
+            <aside>
+                <img src="./assets/images/Peor-mejor.jpg" alt="">
+                <h2>Top de la peor a la mejor mazmorra en Ocarina of Time</h2>
+                <p>Ocarina of Time tiene varias mazmorras a lo largo de nuestra aventura en Hyrule, acompañanos a decidir y conocer cuáles son las mejores y peores mazmorras según nuestro criterio.</p>
+                <a href="peor-mejor-ocarina.php"><button class="btn btn-primary">leer más</button></a>
+            </aside>
+
+            <aside>
+                <img src="./assets/images/Triste.jpg" alt="">
+                <h2>La triste verdad de Ocarina of Time</h2>
+                <p>Quién diría que de un juego tan vivo y colorido, habría cierta profundidad que albergaría en una duda y un dilema para nuestro héroe. </p>
+                <a href="lado-oscuro-ocarina.php"><button class="btn btn-primary">leer más</button></a>
+                
+            </aside>
+
+            <aside>
+                <img src="./assets/images/Filtracion.jpg" alt="">
+                <h2>Lo que están haciendo con Tears of The Kingdom ya es insólito</h2>
+                <p>Las cosas no sientan nada bien para Nintendo y todos los que esperamos el Tears of the Kingdom. Todo se ha salido de control.</p>
+                <a href="filtracion-zelda.php"><button class="btn btn-primary">leer más</button></a>
+            </aside>
+
+        </div>
+      </div>
+
+
+
+  </main>
+
+
+
+
+
+  <!--
+    - #FOOTER
+  -->
+
+  <?php include('footer.php') ?>
+
+
+
+
+
+  <!--
+    - custom js link
+  -->
+  <script src="./assets/js/script.js"></script>
+
+  <!--
+    - ionicon link
+  -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <script src="assets/js/header.js"></script>
+</body>
+
+</html>
